8480b8704b700cee80998f0d1221fe4d7992ce4c
jirkakrepl <jiri.krepl@gmail.com>
2022-05-14 16:15:42 +0200

    chore: eslint config

--- index.mjs
+++ index.mjs
@@ -1,21 +1,23 @@
 import fetch from 'node-fetch';
 import xpath from 'xpath';
 import { DOMParser } from 'xmldom';
-import express from "express";
+import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
+// eslint-disable-next-line no-underscore-dangle
 const __filename = fileURLToPath(import.meta.url);
+// eslint-disable-next-line no-underscore-dangle
 const __dirname = path.dirname(__filename);
 
 const app = express();
 const port = process.env.PORT || 8080;
-app.get('/', function(req, res) {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/index.html'));
 });
 
 app.listen(port);
-console.log('Server started at http://localhost:' + port);
+console.log(`Server started at http://localhost:${port}`);
 
 const response = await fetch('https://www.bazen-olomouc.cz/');
 const body = await response.text();
