e93842874e2231d561471c5d7752082e8b8ea511
jirkakrepl <jiri.krepl@gmail.com>
2022-05-14 14:14:53 +0200

    feat: parse html dom and extract number of people with xpath

--- index.mjs
+++ index.mjs
@@ -1,6 +1,14 @@
 import fetch from 'node-fetch';
+import xpath from 'xpath';
+import { DOMParser }from 'xmldom';
 
 const response = await fetch('https://www.bazen-olomouc.cz/');
 const body = await response.text();
 
-console.log(body);
+// console.log(body);
+
+const doc = new DOMParser({ errorHandler: { warning: null } }).parseFromString(body);
+const nodes = xpath.select('//header[@class="header"]//ul/li[2]/strong', doc)
+
+console.log(nodes[0].localName + ": " + nodes[0].firstChild.data)
+console.log("Node: " + nodes[0].toString())
