79c12e9f2739c84eaefda9ff7e0be9917bb5ecc9
jirkakrepl <jiri.krepl@gmail.com>
2022-05-14 17:56:16 +0200

    feat: open/close browsers according to visitors count
    - count delta of visitors and open or close browsers
    
    - refresh all browser pages every minute

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-await-in-loop */
 import fetch from 'node-fetch';
 import xpath from 'xpath';
 import { DOMParser } from 'xmldom';
@@ -6,6 +7,22 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import puppeteer from 'puppeteer';
 import { sleep } from './utils.js';
+
+/**
+ * fetch swimming pool website html and get number of swimmers with xpath query
+ */
+async function getVisitorsCount() {
+  const response = await fetch('https://www.bazen-olomouc.cz/');
+  const body = await response.text();
+  // console.log(body);
+
+  const doc = new DOMParser({ errorHandler: { warning: null } }).parseFromString(body);
+  const nodes = xpath.select('//header[@class="header"]//ul/li[2]/strong', doc);
+
+  // console.log(`${nodes[0].localName}: ${nodes[0].firstChild.data}`);
+  // console.log(`Node: ${nodes[0].toString()}`);
+  return nodes[0].firstChild.data;
+}
 
 (async () => {
   // eslint-disable-next-line no-underscore-dangle
@@ -22,27 +39,42 @@ import { sleep } from './utils.js';
   app.listen(port);
   console.log(`Server started at http://localhost:${port}`);
 
-  const response = await fetch('https://www.bazen-olomouc.cz/');
-  const body = await response.text();
-  // console.log(body);
+  const browsers = [];
+  let lastVisitorsCount = 0;
 
-  const doc = new DOMParser({ errorHandler: { warning: null } }).parseFromString(body);
-  const nodes = xpath.select('//header[@class="header"]//ul/li[2]/strong', doc);
+  setInterval(async () => {
+    const visitorsCount = await getVisitorsCount();
+    console.log(`visitorsCount at ${new Date().getHours()}:${new Date().getMinutes()} is ${visitorsCount}`);
 
-  console.log(`${nodes[0].localName}: ${nodes[0].firstChild.data}`);
-  console.log(`Node: ${nodes[0].toString()}`);
+    const delta = visitorsCount - lastVisitorsCount;
+    lastVisitorsCount = visitorsCount;
+    console.log(`delta is ${delta}`);
 
-  for (let i = 0; i < 20; i += 1) {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.setUserAgent('Mozilla/5.0 (Linux x86_64; rv:99.0) Gecko/20100101 Firefox/99.0');
-    await page.goto(`http://localhost:${port}`);
-    console.log('opened page', i);
-    await sleep(1000);
-  }
-  // const page = await browser.newPage();
-  // await page.setUserAgent('Mozilla/5.0 (Linux x86_64; rv:99.0) Gecko/20100101 Firefox/99.0');
-  // await page.goto(`http://localhost:${port}`);
-  // await page.screenshot({ path: 'example.png' });
-  // await browser.close();
+    // open browsers for new visitors
+    if (delta > 0) {
+      for (let i = 0; i < delta; i += 1) {
+        console.log('opening new browser n.:', i + 1);
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.setUserAgent('Mozilla/5.0 (Linux x86_64; rv:99.0) Gecko/20100101 Firefox/99.0');
+        await page.goto(`http://localhost:${port}`);
+        browsers.push({ browser, page });
+        await sleep(250); // try to avoid possible GA rate limiter
+      }
+    }
+    // close browsers of left visitors
+    if (delta < 0) {
+      for (let i = 0; i < Math.abs(delta); i += 1) {
+        console.log('closing browser n.:', i + 1);
+        const { browser } = browsers.pop();
+        await browser.close();
+      }
+    }
+    // refresh all browsers
+    console.log('should refresh some browsers');
+    for (const { page } of browsers) {
+      page.reload();
+      await sleep(250); // try to avoid possible GA rate limiter
+    }
+  }, 1000 * 180); // every 2.5 minutes
 })();
