c3722ceb38ee643527835987d9f6ed99bcc687fc
jirkakrepl <jiri.krepl@gmail.com>
2022-05-14 16:05:01 +0200

    feat: create GA property and server index.html page with express
    - on index.html page there is GA code connected to
    bazen olomouc property

--- index.mjs
+++ index.mjs
@@ -1,10 +1,24 @@
 import fetch from 'node-fetch';
 import xpath from 'xpath';
 import { DOMParser } from 'xmldom';
+import express from "express";
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const port = process.env.PORT || 8080;
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+app.listen(port);
+console.log('Server started at http://localhost:' + port);
 
 const response = await fetch('https://www.bazen-olomouc.cz/');
 const body = await response.text();
-
 // console.log(body);
 
 const doc = new DOMParser({ errorHandler: { warning: null } }).parseFromString(body);
