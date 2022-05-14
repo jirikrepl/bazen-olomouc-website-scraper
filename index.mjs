6994bd4b0a91636b22161371ad31a825bf6297c9
jirkakrepl <jiri.krepl@gmail.com>
2022-05-14 16:23:22 +0200

    feat: add pupeteer browser

--- index.mjs
+++ index.mjs
@@ -4,27 +4,36 @@ import { DOMParser } from 'xmldom';
 import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import puppeteer from 'puppeteer';
 
-// eslint-disable-next-line no-underscore-dangle
-const __filename = fileURLToPath(import.meta.url);
-// eslint-disable-next-line no-underscore-dangle
-const __dirname = path.dirname(__filename);
+(async () => {
+  // eslint-disable-next-line no-underscore-dangle
+  const __filename = fileURLToPath(import.meta.url);
+  // eslint-disable-next-line no-underscore-dangle
+  const __dirname = path.dirname(__filename);
 
-const app = express();
-const port = process.env.PORT || 8080;
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/index.html'));
-});
+  const app = express();
+  const port = process.env.PORT || 8080;
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/index.html'));
+  });
 
-app.listen(port);
-console.log(`Server started at http://localhost:${port}`);
+  app.listen(port);
+  console.log(`Server started at http://localhost:${port}`);
 
-const response = await fetch('https://www.bazen-olomouc.cz/');
-const body = await response.text();
-// console.log(body);
+  const response = await fetch('https://www.bazen-olomouc.cz/');
+  const body = await response.text();
+  // console.log(body);
 
-const doc = new DOMParser({ errorHandler: { warning: null } }).parseFromString(body);
-const nodes = xpath.select('//header[@class="header"]//ul/li[2]/strong', doc);
+  const doc = new DOMParser({ errorHandler: { warning: null } }).parseFromString(body);
+  const nodes = xpath.select('//header[@class="header"]//ul/li[2]/strong', doc);
 
-console.log(`${nodes[0].localName}: ${nodes[0].firstChild.data}`);
-console.log(`Node: ${nodes[0].toString()}`);
+  console.log(`${nodes[0].localName}: ${nodes[0].firstChild.data}`);
+  console.log(`Node: ${nodes[0].toString()}`);
+
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(`http://localhost:${port}`);
+  // await page.screenshot({ path: 'example.png' });
+  // await browser.close();
+})();
