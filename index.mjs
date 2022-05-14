5460e381092277658760c6cd83c43d5fe366e218
jirkakrepl <jiri.krepl@gmail.com>
2022-05-14 14:34:57 +0200

    chore: add eslint, lint index.js

--- index.mjs
+++ index.mjs
@@ -1,6 +1,6 @@
 import fetch from 'node-fetch';
 import xpath from 'xpath';
-import { DOMParser }from 'xmldom';
+import { DOMParser } from 'xmldom';
 
 const response = await fetch('https://www.bazen-olomouc.cz/');
 const body = await response.text();
@@ -8,7 +8,7 @@ const body = await response.text();
 // console.log(body);
 
 const doc = new DOMParser({ errorHandler: { warning: null } }).parseFromString(body);
-const nodes = xpath.select('//header[@class="header"]//ul/li[2]/strong', doc)
+const nodes = xpath.select('//header[@class="header"]//ul/li[2]/strong', doc);
 
-console.log(nodes[0].localName + ": " + nodes[0].firstChild.data)
-console.log("Node: " + nodes[0].toString())
+console.log(`${nodes[0].localName}: ${nodes[0].firstChild.data}`);
+console.log(`Node: ${nodes[0].toString()}`);
